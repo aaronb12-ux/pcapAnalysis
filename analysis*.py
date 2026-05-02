a3f8c1298c080861034d030fa18a37118f7959b4
aaronb12-ux <afairster03@gmail.com>
2026-05-02 16:44:08 -0700

    did questions 7 and 8

--- analysis*.py
+++ analysis*.py
@@ -81,10 +81,67 @@ print("Question 4:", https_count)
 print("Question 5:", bestipAdress)
 
 
+http_count_forever = 0
+https_count_forever = 0
+with open('http_forever.pcap', 'rb') as f:
+
+      pcap = dpkt.pcap.Reader(f)
+
+      for timestamp, buf in pcap:
+          
+          eth = dpkt.ethernet.Ethernet(buf) #parsing and decoding packet data into eth object
+
+          if isinstance(eth.data, dpkt.ip.IP):
+              
+              ip = eth.data
+
+              if isinstance(ip.data, dpkt.tcp.TCP):
+                  
+                  tcp = ip.data
+
+                  if tcp.sport == 80 or tcp.dport == 80:
+                    http_count_forever += 1
+
+                  elif tcp.sport == 443 or tcp.dport == 443:
+                    https_count_forever += 1
+
+print("Question 6:", http_count_forever)
+print("Question 7:", https_count_forever)
+          
+          
+with open('ftp.pcap', 'rb') as f:
+
+    pcap = dpkt.pcap.Reader(f)
+
+    for timestamp, buf in pcap:
+
+        eth = dpkt.ethernet.Ethernet(buf)
+
+        if isinstance(eth.data, dpkt.ip.IP):
+
+            ip = eth.data
+
+            if isinstance(ip.data, dpkt.tcp.TCP):
+
+                tcp = ip.data
+
+                if tcp.sport == 21:
+                    ipAdd = socket.inet_ntoa(ip.src)
+                
+                elif tcp.dport == 21:
+                    ipAdd = socket.inet_ntoa(ip.dst)
+        
+
+print("Question 8:", ipAdd)
+
 '''
 parsing PCAP file: https://jon.oberheide.org/blog/2008/10/15/dpkt-tutorial-2-parsing-a-pcap-file/
 
 for HTTP: the client dst is 80 and the server src is 80
 for HTTPS: the client dst is 443 and the server src is 443
+
+
+
+Port 21 is the official, standardized port that FTP servers use for communication
 
 '''
